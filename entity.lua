c720c820791c73ae1f3a4f4c9a50f8eca0650ef2
Hattiel <hattie.lloyd@gmail.com>
2012-04-08 18:15:56 +0930

    entities exist, spacebar resets position

--- entity.lua
+++ entity.lua
@@ -6,17 +6,36 @@ local manager = {
 }
 
 M.new = function (arg)
-  local self = {
-    x = arg.x,
-    y = arg.y
-  }
+  local self = arg
   
   local object = {
     draw = function ()
-      love.graphics.circle("fill", self.x, self.y, 16, 16)
+      love.graphics.circle("fill", self.position.x, self.position.y, 16, 16)
     end,
     update = function (dt)
-      
+      self.velocity.x = self.velocity.x + dt*self.acceleration.x
+      self.velocity.y = self.velocity.y + dt*self.acceleration.y
+      self.position.x = self.position.x + dt*self.velocity.x
+      self.position.y = self.position.y + dt*self.velocity.y
+    end,
+    position = function (arg)
+      self.position.x = arg.x or self.position.x
+      self.position.y = arg.y or self.position.y
+      return self.position
+    end,
+    velocity = function (arg)
+      self.velocity.x = arg.x or self.velocity.x
+      self.velocity.y = arg.y or self.velocity.y
+      return self.velocity
+    end,
+    acceleration = function (arg)
+      self.acceleration.x = arg.x or self.acceleration.x
+      self.acceleration.y = arg.y or self.acceleration.y
+      return self.acceleration
+    end,
+    bearing = function ()
+      local hypotenuse = math.sqrt( math.pow(self.velocity.x,2) + math.pow(self.velocity.y,2) )
+      return math.acos( self.velocity.x/hypotenuse )
     end
   }
   table.insert(manager.entities, object)
@@ -35,5 +54,9 @@ M.update = function (dt)
     entity.update(dt)
   end
 end
+
+
+
+
 
 return M
--- game/main.lua
+++ game/main.lua
@@ -8,10 +8,27 @@ context.map = function (raw_input, map_input)
   return raw_input, map_input
 end
 
+
+local dude = gauge.entity.new{
+  position = { x = 200, y = 200 },
+  velocity = { x = 100, y = 0 },
+  acceleration = { x = 0, y = 0},
+}
+dude.lifetime = 0
+
+local update = dude.update
+dude.update = function (dt)
+  update(dt)
+  dude.lifetime = dude.lifetime + dt
+end
+
+
 gauge.event.subscribe("input",
   function (input)
     if input.actions.jump then
       -- do something
+      dude.position({x = 100, y = 100})
     end
   end
 )
+
