099f516ec3498aed3266cdd0b52d6e0499878766
Saxon Douglass <saxon@saxondouglass.com>
2012-04-08 11:54:57 +0930

    added entity.lua

---
+++ entity.lua
@@ -0,0 +1,39 @@
+-- entity.lua
+local M = {}
+
+local manager = {
+  entities = {}
+}
+
+M.new = function (arg)
+  local self = {
+    x = arg.x,
+    y = arg.y
+  }
+  
+  local object {
+    draw = function ()
+      love.graphics.circle("fill", self.x, self.y, 16, 16)
+    end,
+    update = function (dt)
+      
+    end
+  }
+  table.insert(manager.entities, object)
+  
+  return object
+end
+
+M.draw = function ()
+  for _,entity in ipairs(manager.entities) do
+    entity.draw()
+  end
+end
+
+M.update = function (dt)
+  for _,entity in ipairs(manager.entities) do
+    entity.update(dt)
+  end
+end
+
+return M
--- main.lua
+++ main.lua
@@ -1,12 +1,42 @@
-local event = require "event"
 local sandbox = require "sandbox"
 
-love.load = function()
-  local t = function()
-    print("hi!")
-  end
-  event.subscribe("test", t)
+local gauge = {}
+gauge.event = require "event"
+gauge.entity = require "entity"
 
-  sandbox.allow("event", event)
+love.load = function ()
+  sandbox.allow("gauge", gauge)
   sandbox.run_file("game/main.lua")
+end
+
+love.update = function (dt)
+  gauge.entity.update(dt)
+end
+
+love.draw = function ()
+  gauge.entity.draw()
+end
+
+love.mousepressed = function (x, y, button)
+
+end
+
+love.mousereleased = function (x, y, button)
+
+end
+
+love.keypressed = function (key, unicode)
+
+end
+
+love.keyreleased = function (key, unicode)
+
+end
+
+love.focus = function (f)
+
+end
+
+love.quit = function ()
+
 end
