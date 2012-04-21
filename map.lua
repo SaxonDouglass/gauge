2931bbfc3ba1792fbe5860a696dda50cb895226a
Saxon Douglass <saxon.douglass@gmail.com>
2012-04-21 16:18:01 +0930

    map.lua now exposes getTile()

--- map.lua
+++ map.lua
@@ -1,40 +1,31 @@
+-- map.lua
 local M = {}
 
 M.new = function(arg)
-    mapfile = love.filesystem.load(levels[level])()
-    
-    map = mapfile.layers[1]
-    
-    map.colour = loadstring("return " .. mapfile.properties.colour)()
+  local map = nil
+  _,map = pcall(arg.data)
 
-    for x=1,map.width do
-        map[x] = {}
-        for y=1,map.height do
-            map[x][y] = map.data[x + (map.width * (y - 1))]
-        end
+  -- map properties
+  for k,v in pairs(map.properties) do
+    local property = assert(loadstring(v))
+    _,map.properties[k] = pcall(property)
+  end
+  
+  -- tileset properties
+  for _,tileset in ipairs(map.tilesets) do
+    local tiles = {}
+    for _,tile in ipairs(tileset.tiles) do
+      tiles[tile.id] = tile.properties
     end
-    
-    map.batch = love.graphics.newSpriteBatch(tileset.image,
-        map.width * map.height)
-    
-    map.batch:clear()
-    for x=1,map.width do
-        for y=1,map.height do
-            if mapGetProp("player", x, y) then
-                map[x][y] = 1
-                player.x = x
-                player.y = y
-                player.frame = 1
-            elseif mapGetProp("enemy", x, y) then
-                map[x][y] = 1
-                enemies.new(x, y)
-            elseif mapGetProp("pip", x, y) then
-                map[x][y] = 1
-                pips.new(x, y)
-            end
-        
-            map.batch:addq(tileset.quads[map[x][y]],
-                (x-1)*tileset.size, (y-1)*tileset.size)
-        end
-    end
+    tileset.tiles = tiles
+  end
+  
+  -- getTile(x, y)
+  M.getTile = function (x, y)
+    local index = x + (y * map.layers[0].width)
+    local tile_id = map.layers[0].data[index]
+    return map.properties[tile_id] || {}
+  end
 end
+
+return M
