7d0f86ec1a9f1c08e9f66457112ad2a35a970644
Saxon Douglass <saxon.douglass@gmail.com>
2012-04-23 23:08:11 +0930

    added extra height to some levels

--- 4.lua
+++ 4.lua
@@ -1,1 +1,331 @@
-return { version = "1.1", luaversion = "5.1", orientation = "orthogonal", width = 25, height = 19, tilewidth = 128, tileheight = 128, properties = {}, tilesets = { { name = "default", firstgid = 1, tilewidth = 128, tileheight = 128, spacing = 0, margin = 0, image = "Magic-Forest.png", imagewidth = 1408, imageheight = 1152, properties = {}, tiles = { { id = 0, properties = { ["solid"] = "true" } }, { id = 1, properties = { ["solid"] = "true" } }, { id = 2, properties = { ["solid"] = "true" } }, { id = 3, properties = { ["solid"] = "true" } }, { id = 4, properties = { ["solid"] = "true" } }, { id = 5, properties = { ["solid"] = "true" } }, { id = 6, properties = { ["solid"] = "true" } }, { id = 7, properties = { ["solid"] = "true" } }, { id = 22, properties = { ["solid"] = "true" } }, { id = 23, properties = { ["solid"] = "true" } }, { id = 24, properties = { ["solid"] = "true" } }, { id = 25, properties = { ["solid"] = "true" } }, { id = 26, properties = { ["solid"] = "true" } }, { id = 29, properties = { ["solid"] = "true" } }, { id = 30, properties = { ["solid"] = "true" } }, { id = 31, properties = { ["solid"] = "true" } }, { id = 32, properties = { ["solid"] = "true" } }, { id = 44, properties = { ["solid"] = "true" } }, { id = 45, properties = { ["solid"] = "true" } }, { id = 46, properties = { ["solid"] = "true" } }, { id = 47, properties = { ["solid"] = "true" } }, { id = 55, properties = { ["solid"] = "true" } }, { id = 56, properties = { ["solid"] = "true" } }, { id = 57, properties = { ["solid"] = "true" } }, { id = 58, properties = { ["solid"] = "true" } }, { id = 66, properties = { ["solid"] = "true" } }, { id = 67, properties = { ["solid"] = "true" } }, { id = 68, properties = { ["solid"] = "true" } }, { id = 69, properties = { ["solid"] = "true" } }, { id = 77, properties = { ["solid"] = "true" } }, { id = 78, properties = { ["solid"] = "true" } }, { id = 79, properties = { ["solid"] = "true" } }, { id = 80, properties = { ["solid"] = "true" } }, { id = 88, properties = { ["solid"] = "true" } }, { id = 89, properties = { ["solid"] = "true" } }, { id = 90, properties = { ["solid"] = "true" } }, { id = 91, properties = { ["solid"] = "true" } } } } }, layers = { { type = "tilelayer", name = "Tile Layer 1", x = 0, y = 0, width = 25, height = 19, visible = true, opacity = 1, properties = {}, encoding = "lua", data = { 48, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47, 59, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 58, 70, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 69, 81, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 80, 48, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47, 59, 29, 20, 21, 22, 49, 49, 29, 28, 20, 21, 22, 21, 28, 29, 49, 49, 49, 49, 49, 49, 49, 49, 49, 58, 70, 67, 23, 23, 46, 49, 49, 67, 23, 23, 23, 23, 23, 23, 46, 49, 49, 49, 49, 45, 23, 23, 23, 23, 69, 81, 89, 49, 49, 49, 49, 49, 78, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47, 48, 49, 49, 49, 80, 48, 49, 49, 49, 49, 40, 41, 78, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 58, 59, 49, 49, 49, 47, 59, 49, 49, 49, 49, 7, 8, 78, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 69, 70, 49, 49, 49, 58, 70, 49, 49, 49, 45, 23, 57, 89, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 80, 81, 49, 49, 49, 69, 81, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47, 48, 49, 49, 49, 80, 92, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 45, 23, 23, 23, 23, 23, 23, 57, 58, 59, 49, 49, 49, 47, 23, 23, 68, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47, 48, 49, 49, 49, 47, 49, 49, 79, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 58, 59, 49, 49, 49, 58, 49, 49, 90, 56, 23, 23, 23, 23, 23, 23, 23, 23, 23, 68, 49, 49, 49, 49, 49, 69, 70, 49, 49, 49, 69, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 79, 49, 49, 49, 49, 49, 80, 81, 49, 49, 49, 80, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 79, 49, 49, 49, 49, 49, 91, 92, 49, 49, 49, 91, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 90, 56, 23, 23, 23, 23, 23, 23, 23, 23, 23, 57 } }, { type = "objectgroup", name = "Object Layer 1", visible = true, opacity = 1, properties = {}, objects = { { name = "", type = "door", x = 2560, y = 384, width = 384, height = 384, properties = {} }, { name = "", type = "player_spawn", x = 256, y = 512, width = 128, height = 128, properties = {} }, { name = "", type = "grower", x = 512, y = 640, width = 128, height = 128, properties = {} }, { name = "", type = "grower", x = 2048, y = 1920, width = 128, height = 128, properties = {} } } } } }
+return {
+  version = "1.1",
+  luaversion = "5.1",
+  orientation = "orthogonal",
+  width = 25,
+  height = 22,
+  tilewidth = 128,
+  tileheight = 128,
+  properties = {},
+  tilesets = {
+    {
+      name = "default",
+      firstgid = 1,
+      tilewidth = 128,
+      tileheight = 128,
+      spacing = 0,
+      margin = 0,
+      image = "Magic-Forest.png",
+      imagewidth = 1408,
+      imageheight = 1152,
+      properties = {},
+      tiles = {
+        {
+          id = 0,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 1,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 2,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 3,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 4,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 5,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 6,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 7,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 22,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 23,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 24,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 25,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 26,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 29,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 30,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 31,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 32,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 44,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 45,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 46,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 47,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 55,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 56,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 57,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 58,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 66,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 67,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 68,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 69,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 77,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 78,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 79,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 80,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 88,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 89,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 90,
+          properties = {
+            ["solid"] = "true"
+          }
+        },
+        {
+          id = 91,
+          properties = {
+            ["solid"] = "true"
+          }
+        }
+      }
+    }
+  },
+  layers = {
+    {
+      type = "tilelayer",
+      name = "Tile Layer 1",
+      x = 0,
+      y = 0,
+      width = 25,
+      height = 22,
+      visible = true,
+      opacity = 1,
+      properties = {},
+      encoding = "lua",
+      data = {
+        48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 47,
+        48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 47,
+        48, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 47,
+        48, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47,
+        59, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 58,
+        70, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 69,
+        81, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 80,
+        48, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47,
+        59, 29, 20, 21, 22, 49, 49, 29, 28, 20, 21, 22, 21, 28, 29, 49, 49, 49, 49, 49, 49, 49, 49, 49, 58,
+        70, 67, 23, 23, 46, 49, 49, 67, 23, 23, 23, 23, 23, 23, 46, 49, 49, 49, 49, 45, 23, 23, 23, 23, 69,
+        81, 89, 49, 49, 49, 49, 49, 78, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47, 48, 49, 49, 49, 80,
+        48, 49, 49, 49, 49, 40, 41, 78, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 58, 59, 49, 49, 49, 47,
+        59, 49, 49, 49, 49, 7, 8, 78, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 69, 70, 49, 49, 49, 58,
+        70, 49, 49, 49, 45, 23, 57, 89, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 80, 81, 49, 49, 49, 69,
+        81, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47, 48, 49, 49, 49, 80,
+        92, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 45, 23, 23, 23, 23, 23, 23, 57, 58, 59, 49, 49, 49, 47,
+        23, 23, 68, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 47, 48, 49, 49, 49, 47,
+        49, 49, 79, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 58, 59, 49, 49, 49, 58,
+        49, 49, 90, 56, 23, 23, 23, 23, 23, 23, 23, 23, 23, 68, 49, 49, 49, 49, 49, 69, 70, 49, 49, 49, 69,
+        49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 79, 49, 49, 49, 49, 49, 80, 81, 49, 49, 49, 80,
+        49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 79, 49, 49, 49, 49, 49, 91, 92, 49, 49, 49, 91,
+        49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 49, 90, 56, 23, 23, 23, 23, 23, 23, 23, 23, 23, 57
+      }
+    },
+    {
+      type = "objectgroup",
+      name = "Object Layer 1",
+      visible = true,
+      opacity = 1,
+      properties = {},
+      objects = {
+        {
+          name = "",
+          type = "door",
+          x = 2560,
+          y = 768,
+          width = 384,
+          height = 384,
+          properties = {}
+        },
+        {
+          name = "",
+          type = "player_spawn",
+          x = 256,
+          y = 896,
+          width = 128,
+          height = 128,
+          properties = {}
+        },
+        {
+          name = "",
+          type = "grower",
+          x = 512,
+          y = 1024,
+          width = 128,
+          height = 128,
+          properties = {}
+        },
+        {
+          name = "",
+          type = "grower",
+          x = 2048,
+          y = 2304,
+          width = 128,
+          height = 128,
+          properties = {}
+        }
+      }
+    }
+  }
+}
